601eec06b53a0505f6b97a29401132d45c193032
Andreas Gruenbacher <agruen@linbit.com>
2013-07-23 18:03:40 +0200

    *

---
+++ client/param.sh
@@ -0,0 +1,36 @@
+# We store all global settings in array[_].
+node=_
+
+# Keep track of how many parameter arrays have already been
+# defined in param_count[array] (e.g., param_count[DISK] is 2
+# if DISK1 and DISK2 have been defined).  We reset param_count
+# for each new node so that the global settings can be overridden.
+declare -A param_count
+
+new_node() {
+    local global
+
+    node=$1
+    shift
+    NODES=("${NODES[@]}" "$node")
+    declare -g -A param_count=()
+
+    # Copy the global settings from array[_] to array[$node]
+    for array in "$@"; do
+	eval "[ -z \"\${$array[_]+x}\" ] || $array[\$node]=\"\${$array[_]}\""
+    done
+}
+
+set_node_param() {
+    local name=$1 node=$2 value=$3
+
+    declare -g -A "$name"
+    eval "$name[\$node]=$value"
+}
+
+add_node_param() {
+    local name=$1 node=$2 value=$3
+    local count=$((++param_count[$name]))
+
+    set_node_param "$name$count" "$node" "$value"
+}

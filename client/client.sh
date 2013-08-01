a436f0ceb226015901df5a74907deca92f138788
Andreas Gruenbacher <agruen@linbit.com>
2013-08-01 14:30:38 +0200

    ***

--- client/client.sh
+++ client/client.sh
@@ -1,3 +1,11 @@
+export DRBD_TEST_DATA=/usr/share/drbd-test
+
+tmpdir=$(mktemp -dt)
+cleanup() {
+    rm -rf "$tmpdir"
+}
+trap cleanup EXIT
+
 do_debug() {
     if [ -n "$opt_debug" ]; then
 	echo -n "#"
@@ -11,28 +19,44 @@ jobdir() {
     echo "$1"
 }
 
-run_exxe_on_node() {
-    local node=$1
+# This is similar to bash's coproc command, except that the coproc command only
+# allows one coprocess at a time, and even then doesn't really seem to work as
+# described.
+create_coprocess() {
+    local proc=$1 in out
+    shift
 
-    [ -z "${NODE_IN[$node]}" ] || return 0
-    # exxe -n -i export DRBD_TEST_JOB=$DRBD_TEST_JOB
-    # exxe -n -i export PATH=...
+    mkfifo $tmpdir/io-$proc $tmpdir/oi-$proc
+    "$@" > $tmpdir/io-$proc < $tmpdir/oi-$proc &
+    exec {in}< $tmpdir/io-$proc {out}> $tmpdir/oi-$proc
+    eval "export ${proc}_PID=$!; ${proc}_IN=$in ${proc}_OUT=$out"
+    rm -f $tmpdir/io-$proc $tmpdir/oi-$proc
 }
 
-on_node() {
-    local node=$1
-
-    run_exxe_on_node "$node"
-    # Make sure that the drbd-test scripts are in $PATH on the server
-    # Log everything we do here ...
-    # FIXME: Pass the job name to the server in the environment
-    :
+close_coprocess() {
+    local proc=$1 out
+    eval "out=${proc}_OUT"
+    eval "exec ${!out}>&-"
+    eval "wait \$${proc}_PID"
 }
 
-on_all_nodes() {
-    local node
+on() {
+    local no_stdin=-n proc procs var
+    if [ "$1" = "-i" ]; then
+	no_stdin=
+	shift
+    fi
+    while :; do
+	var=$1_PID
+	[ -n "${!var}" ] || break
+	procs[${#procs[@]}]=$1
+	shift
+    done
 
-    for node in "${NODES[@]}"; do
-	on_node "$node" "$@"
+    for proc in "${procs[@]}"; do
+	eval "exxe $no_stdin -i \"\$@\" >&\$${proc}_OUT"
+    done
+    for proc in "${procs[@]}"; do
+	eval "exxe -o <&\$${proc}_IN"
     done
 }
--- client/coprocess.sh
+++ client/coprocess.sh
@@ -1,52 +1,6 @@
 #! /bin/bash
 
-tmpdir=$(mktemp -dt)
-cleanup() {
-    rm -rf "$tmpdir"
-}
-trap cleanup EXIT
-
-# This is similar to bash's coproc command, except that the coproc command only
-# allows one coprocess at a time, and even then doesn't really seem to work as
-# described.
-create_coprocess() {
-    local name=$1 in out
-    shift
-
-    mkfifo $tmpdir/io-$name $tmpdir/oi-$name
-    "$@" > $tmpdir/io-$name < $tmpdir/oi-$name &
-    exec {in}< $tmpdir/io-$name {out}> $tmpdir/oi-$name
-    eval "${name}_PID=$!; $name=($in $out)"
-    rm -f $tmpdir/io-$name $tmpdir/oi-$name
-}
-
-close_coprocess() {
-    local name=$1 out
-    eval "out=$name[1]"
-    eval "exec ${!out}>&-"
-    eval "wait \$${name}_PID"
-}
-
-on() {
-    local no_stdin=-n proc procs var
-    if [ "$1" = "-i" ]; then
-	no_stdin=
-	shift
-    fi
-    while :; do
-	var=$1_PID
-	[ -n "${!var}" ] || break
-	procs[${#procs[@]}]=$1
-	shift
-    done
-
-    for proc in "${procs[@]}"; do
-	eval "exxe $no_stdin -i \"\$@\" >&\${$proc[1]}"
-    done
-    for proc in "${procs[@]}"; do
-	eval "exxe -o <&\${$proc[0]}"
-    done
-}
+. client.sh
 
 create_coprocess LOCAL exxe
 create_coprocess X ssh localhost exxe

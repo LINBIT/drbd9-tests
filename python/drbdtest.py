6656afe21b334edc601f65c385bbca25d72d90dd
Philipp Marek <philipp.marek@linbit.com>
2015-06-01 09:04:03 +0200

    Use a ".txt" extension, so that jenkins sends the correct mime type.

--- python/drbdtest.py
+++ python/drbdtest.py
@@ -1109,7 +1109,7 @@ def setup(parser=argparse.ArgumentParser(),
 
     syslog_port = 5140
     syslog_server(args.node, port=syslog_port,
-                  acc_name=os.path.join(args.logdir, 'syslog.full'),
+                  acc_name=os.path.join(args.logdir, 'syslog.full.txt'),
                   logfile_name=os.path.join(args.logdir, 'syslog-%s'))
     resource.nodes.run(['rsyslogd', socket.gethostname(), str(syslog_port)],
                        prepare=True)

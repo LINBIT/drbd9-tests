3a11c416e1b3f5455318a9dfd8c8241c2d40fa6e
Joel Colledge <joel.colledge@linbit.com>
2020-10-14 12:25:18 +0200

    virter: add ci_enabled field to elasticsearch data
    This indicates whether a test is included in the standard set of tests
    run on merge requests.

--- virter/elasticsearch/elasticsearch-insert.sh
+++ virter/elasticsearch/elasticsearch-insert.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-if [ "$#" -ne 2 ]; then
-	echo "Usage: $0 elasticsearch_url results_file"; exit 1
+if [ "$#" -ne 3 ]; then
+	echo "Usage: $0 elasticsearch_url results_file ci_tests_file"; exit 1
 fi
 
 url="$1"
-file="$2"
+results_file="$2"
+ci_tests_file="$3"
 
-date="$(cat $file | jq -r .time | head -1)"
+date="$(cat $results_file | jq -r .time | head -1)"
 
 if [ -z "$date" ]; then
 	echo "No data to insert"; exit 1
@@ -37,16 +38,24 @@ if [ "$index_exists" = false ]; then
 				"status": { "type": "keyword" },
 				"score": { "type": "integer" },
 				"duration_ns": { "type": "long" },
-				"name_count": { "type": "keyword" }
+				"name_count": { "type": "keyword" },
+				"ci_enabled": { "type": "boolean" }
 			}
 		}
 	}
 	'
 fi
 
-cat "$file" | jq -c '
+ci_tests="$(cat "$ci_tests_file" | rq -t | jq -c '.tests | to_entries | map(.key + "-" + (.value.vms[] | tostring))')"
+
+echo "CI Tests: $ci_tests"
+
+cat "$results_file" | jq -c '
 [
 	{ index: { _id: .id } },
-	del(.id) + { name_count: (.name + "-" + (.vm_count | tostring)) }
+	(del(.id) |
+		(.name + "-" + (.vm_count | tostring)) as $nc |
+		.name_count = $nc |
+		.ci_enabled = ('"$ci_tests"' | index($nc) != null))
 ][]
 ' | curl -f "$url/$index/_bulk?pretty" -H "Content-Type: application/x-ndjson" -XPOST --data-binary @-

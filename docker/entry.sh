5f67c6635caca3208c23151831eea7ced53a4569
Joel Colledge <joel.colledge@linbit.com>
2021-03-10 16:10:18 +0100

    ci: remove SSH key handling
    Virter now does this for us.

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ -z "$TEST_NAME" ]; then
     echo "No test specified"
     exit 1
@@ -10,22 +12,16 @@ if [ ! -e tests/"$TEST_NAME" ]; then
     exit 1
 fi
 
-echo "===== Prepare SSH access"
-
-mkdir -p /root/.ssh
-echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
+echo "===== Get the FQDN from the test nodes"
 
 nodes=()
 
 IFS=,; for t in $TARGETS; do
-    ssh-keyscan -H $t >> ~/.ssh/known_hosts
-
     t_host=$(ssh $t hostname -f)
     echo "=== Target $t => $t_host"
-    echo "$t $t_host" >> /etc/hosts
-    ssh-keyscan -H $t_host >> ~/.ssh/known_hosts
     nodes+=( "$t_host" )
 done
+
+echo "===== Run test '$TEST_NAME' with nodes: ${nodes[@]}"
 
 tests/$TEST_NAME --no-syslog --logdir /log ${nodes[@]}

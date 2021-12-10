d398395017dda0ac855c63b18b29ed90bc568edb
Joel Colledge <joel.colledge@linbit.com>
2021-12-10 09:50:25 +0100

    ci: save output from base image builds to artifacts
    For each base image a separate file will be saved. The line buffered
    output is useful for following the job actively, but this is easier to
    interpret retrospectively.

---
+++ virter/build-push-base-image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+die() {
+	echo "$1" >&2
+	exit 1
+}
+
+[ "$#" -ne 2 ] && die "Usage: $0 index base_image"
+
+index="$1"
+base_image="$2"
+
+[ -z "$LINBIT_DOCKER_REGISTRY" ] && die "Missing \$LINBIT_DOCKER_REGISTRY"
+
+virter_image_name="build-$base_image"
+id=$((100 + $index))
+
+virter image rm "$virter_image_name" || die "could not remove image before build"
+make "base_image_$base_image" BASE_IMAGE_NAME="$virter_image_name" VIRTER_BUILD_ID=$id || die "could not build"
+virter image push "$virter_image_name" "$LINBIT_DOCKER_REGISTRY/vm/drbd9-tests/$base_image:latest" || die "could not push"
+virter image rm "$virter_image_name" || die "could not remove image after build"

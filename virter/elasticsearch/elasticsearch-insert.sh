3acdc4744294c6760fc013cf21719a26fb129d13
Joel Colledge <joel.colledge@linbit.com>
2021-12-20 16:43:07 +0100

    virter: use one elasticsearch index per month
    Elasticsearch has a limit on the number of indices that may be created.
    It is also not good for performance to split the data over too many
    indices. So store all results from one month in one index.

--- virter/elasticsearch/elasticsearch-insert.sh
+++ virter/elasticsearch/elasticsearch-insert.sh
@@ -19,12 +19,7 @@ ci_tests_file="$3"
 [ -z "$DRBD9_TESTS_VERSION" ] && die "Missing \$DRBD9_TESTS_VERSION"
 [ -z "$DRBD9_TESTS_REF" ] && die "Missing \$DRBD9_TESTS_REF"
 
-date="$(cat $results_file | jq -r .time | head -1)"
-[ -z "$date" ] && die "No data to insert"
-
-# the index should uniquely identify the test suite run because _id is only
-# unique within a test suite run
-index="drbd-$(date --date=$date --utc +%Y%m%d%H%M%S)-$CI_JOB_ID"
+index="drbd-$(date --utc +%Y%m)"
 
 echo "Using index: '$index'"
 
@@ -37,6 +32,7 @@ if [ "$index_exists" = false ]; then
 		"mappings": {
 			"properties": {
 				"job_id": { "type": "keyword" },
+				"test_run_id": { "type": "keyword" },
 				"drbd_version": { "type": "keyword" },
 				"drbd_utils_version": { "type": "keyword" },
 				"drbd9_tests_version": { "type": "keyword" },
@@ -62,19 +58,18 @@ ci_tests="$(cat "$ci_tests_file" | rq -t | jq -c '.tests | to_entries | map(.key
 echo "CI Tests: $ci_tests"
 
 cat "$results_file" | jq -c '
-[
-	{ index: { _id: .id } },
-	(del(.id) |
-		(.name + "-" + (.vm_count | tostring)) as $nc |
-		. + {
-			job_id: "'"$CI_JOB_ID"'",
-			drbd_version: "'"$DRBD_VERSION"'",
-			drbd_utils_version: "'"$DRBD_UTILS_VERSION"'",
-			drbd9_tests_version: "'"$DRBD9_TESTS_VERSION"'",
-			drbd9_tests_ref: "'"$DRBD9_TESTS_REF"'",
-			name_count: $nc,
-			ci_enabled: ('"$ci_tests"' | index($nc) != null),
-		}
-	)
-][]
+{ index: { _id: ( "'"$CI_JOB_ID-"'" + .id ) } },
+(
+	(.name + "-" + (.vm_count | tostring)) as $nc |
+	del(.id) + {
+		job_id: "'"$CI_JOB_ID"'",
+		test_run_id: .id,
+		drbd_version: "'"$DRBD_VERSION"'",
+		drbd_utils_version: "'"$DRBD_UTILS_VERSION"'",
+		drbd9_tests_version: "'"$DRBD9_TESTS_VERSION"'",
+		drbd9_tests_ref: "'"$DRBD9_TESTS_REF"'",
+		name_count: $nc,
+		ci_enabled: ('"$ci_tests"' | index($nc) != null),
+	}
+)
 ' | curl -f "$url/$index/_bulk" -H "Content-Type: application/x-ndjson" -XPOST --data-binary @- > /dev/null

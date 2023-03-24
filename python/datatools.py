7632857cac967b2c2da0d31d48140884e3912eb0
Joel Colledge <joel.colledge@linbit.com>
2023-03-24 13:48:16 +0100

    datatools: add explicit primary/secondary for DRBD 8.4
    Also set pipefail so that we notice if we are not able to read the data.

--- python/datatools.py
+++ python/datatools.py
@@ -1,6 +1,12 @@
 from .drbdtest import log
 
 
+def _read_md5sum(node, filename, count_arg):
+    return node.run(['/bin/bash', '-c', 'set -o pipefail ; dd if={} bs=1M iflag=direct {} | md5sum'
+        .format(filename, count_arg)],
+        return_stdout=True)
+
+
 def verify_data(nodes, size_mb=None, backing_disk=False):
     """ Verify that DRBD devices contain the same data. """
 
@@ -14,10 +20,14 @@ def verify_data(nodes, size_mb=None, backing_disk=False):
 
     md5sums=[]
     for n in nodes:
-        md5sum = n.run(['/bin/bash', '-c', 'dd if={} bs=1M iflag=direct {} | md5sum'
-            .format(n.volumes[0].disk if backing_disk else n.volumes[0].device(), count_arg)],
-            return_stdout=True)
-        md5sums.append(md5sum)
+        filename = n.volumes[0].disk if backing_disk else n.volumes[0].device()
+
+        if n.host.drbd_version_tuple < (9, 0, 0):
+            n.primary()
+            md5sums.append(_read_md5sum(n, filename, count_arg))
+            n.secondary()
+        else:
+            md5sums.append(_read_md5sum(n, filename, count_arg))
 
     if len(set(md5sums)) > 1:
         log(md5sums)

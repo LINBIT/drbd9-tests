LINBIT/drbd9-tests

commit 997a3d176e2212702bfbac84d2b09fef3921e26c

Remove m4 config file generation with some python code.
Also included: multi-path setup via "multi_paths" option; for that
the VMs need to have an additional "eth0:1" interface.

python/drbdtest.py
@@ -28,6 +28,7 @@ from subprocess import CalledProcessError
 import atexit
 from ordered_set import OrderedSet
 import exxe
+
 from syslog import syslog_server
 from cStringIO import StringIO
 
@@ -454,14 +455,11 @@ Volumes.finish()
 
 
 class Resource(object):
-    def __init__(self, name, logdir, template=None):
+    def __init__(self, name, logdir):
         self.name = name
         self.net_options = ""
         self.nodes = Nodes()
         self.num_volumes = 0
-        if template is None:
-            template = os.path.join(TOP, 'lib', 'm4', 'template.conf.m4')
-        self.template = template
         self.logdir = logdir
         self.events_cls = None
         self.forbidden_patterns = OrderedSet()
@@ -494,18 +492,6 @@ class Resource(object):
                 node.add_disk(volume, size, meta_size)
             else:
                 node.add_disk(volume)
-
-    @staticmethod
-    def m4_define(name, value):
-        if not isinstance(value, basestring):
-            value = ", ".join(["`" + v + "'" for v in value])
-        return "m4_define(`" + name + "', `" + str(value) + "')\n"
-
-    @staticmethod
-    def m4_define_array(name, dict):
-        return "m4_define_array(`" + name + "')\n" + \
-            "".join([name + "(`" + str(key) + "', `" +
-                     str(value) + "')\n" for key, value in dict.iteritems()])
 
     volumes = property(lambda self: self.nodes.volumes)
     connections = property(lambda self: self.nodes.connections)
@@ -734,8 +720,60 @@ class PeerDevice(object):
         return PeerDevices([self]).event(*args, **kwargs)
 
 
+class ConfigBlock(object):
+    INDENT = "     "
+
+    all_text = ""
+
+    def __init__(self, parent=None, fh=None, fn=None, t="", dest_fn=None):
+        self.parent = parent
+        self.name = t
+        self.fd = None
+        self.to_var = dest_fn
+
+        if self.parent:
+            self.indent = self.parent.indent + self.INDENT
+            self.to_var = self.parent.to_var
+            self.fd = self.parent.fd
+            self.do_close = False
+        else:
+            if fh:
+                self.fd = fh
+                self.do_close = False
+            elif fn:
+                self.fd = open(fn, "wt")
+                self.do_close = True
+
+            self.indent = ""
+
+    def __enter__(self):
+        self.write_no_indent("%s%s {\n" % (self.indent, self.name))
+        return self
+
+    def __exit__(self, *ignore_exception):
+        self.write_no_indent("%s}\n\n" % self.indent)
+
+        if self.fd and not self.parent and self.do_close:
+            self.fd.close()
+
+    def write_no_indent(self, content):
+        if self.to_var:
+            return self.to_var(content)
+        else:
+            return self.fd.write(content)
+
+    def write(self, data, *args):
+        content = "%s%s%s" % (self.indent, self.INDENT, (data % args))
+        if not content.endswith("\n"):
+            content = content + "\n"
+
+        self.write_no_indent(content)
+
+
 class Node(exxe.Exxe):
-    def __init__(self, resource, name, volume_group, addr=None, port=7789):
+    def __init__(self, resource, name, volume_group,
+            addr=None, port=7789,
+            multi_paths=None):
         super(Node, self).__init__(['ssh', '-l',
                                     'root', name,
                                     'exxe', '--syslog'], prefix='%s: ' % name)
@@ -776,8 +814,22 @@ class Node(exxe.Exxe):
                                 prepare=True)
         self.drbd_major_version = int(re.sub(r'\..*', '', drbd_version))
 
-    def addr_port(self):
-        return '%s:%s' % (self.addr, self.port)
+        self.addrs = [self.addr]
+        if multi_paths:
+            net_2 = self.run(['ip', '-oneline', 'a', 'show', 'label', 'eth0:1'],
+                    return_stdout=True)
+            verbose("got further path %s", net_2)
+            m = re.search(r'^\s*\d+:\s+\w+\s+inet\s+([\d\.]+)/\d+', net_2)
+            if not m:
+                raise RuntimeError("%s has no eth0:1", self)
+            self.addrs.append(m.group(1))
+
+        self.run(["bash", "-c", 'iptables -N drbd-test-input && iptables -A INPUT -j drbd-test-input || true'])
+        self.run(["bash", "-c", 'iptables -N drbd-test-output && iptables -A OUTPUT -j drbd-test-output || true'])
+
+
+    def addr_port(self, net_num=0):
+        return '%s:%s' % (self.addrs[net_num], self.port)
 
     def cleanup(self):
         self.config_changed = False
@@ -804,57 +856,72 @@ class Node(exxe.Exxe):
         self.disks.append(Volume(self, volume, size, meta_size))
         self.config_changed = True
 
-    def tmpl_defs(self, resource):
-        devices = []
-        disks = []
-        metas = []
-        for volume in range(resource.num_volumes):
-            disks.append({})
-            metas.append({})
-            devices.append({})
-            for node in resource.nodes:
-                if volume < len(node.disks):
-                    disk = node.disks[volume]
-                    devices[volume][node.name] = '/dev/drbd%d' % disk.minor
-                    disks[volume][node.name] = \
-                        disk.disk if disk.disk is not None else 'none'
-                    if disk.meta is not None:
-                        metas[volume][node.name] = disk.meta
+    def _config_conns_84(self, parent):
+        # no explicit connections for 8.4
+        # done via "address" in "on <host>" section
+        pass
 
-        NODE = []
-        for node in resource.nodes:
-            NODE.append(node.name)
+    def _config_one_host_addr(self, node, block, i):
+        block.write("host %s address %s:%d;" %
+                    (node.name,
+                     node.addrs[i],
+                     node.port))
 
-        return \
-            Resource.m4_define('DRBD_MAJOR_VERSION', str(self.drbd_major_version)) + \
-            Resource.m4_define('RESOURCE', resource.name) + \
-            Resource.m4_define('NODES', NODE) + \
-            Resource.m4_define('NET', resource.net_options) + \
-            Resource.m4_define('VOLUMES', [str(v) for v in xrange(resource.num_volumes)]) + \
-            Resource.m4_define_array('NODE',
-                                     dict((key, value) for key, value in enumerate(NODE))) + \
-            Resource.m4_define_array('NODE_ID',
-                                     dict((value, key) for key, value in enumerate(NODE))) + \
-            ''.join([Resource.m4_define_array('DEVICE%d' % (idx + 1), device)
-                    for idx, device in enumerate(devices) if len(device)]) + \
-            ''.join([Resource.m4_define_array('DISK%d' % (idx + 1), disk)
-                    for idx, disk in enumerate(disks) if len(disk)]) + \
-            ''.join([Resource.m4_define_array('META%d' % (idx + 1), meta)
-                    for idx, meta in enumerate(metas) if len(meta)]) + \
-            Resource.m4_define_array(
-                'HOSTNAME', dict((node.name, node.hostname)
-                    for node in resource.nodes)) + \
-            Resource.m4_define_array(
-                'ADDRESS', dict((node.name, node.addr + ':' + str(node.port))
-                    for node in resource.nodes))
+    def _config_one_connection(self, n1, n2, parent):
+        with ConfigBlock(parent=parent, t="connection") as c:
+            with ConfigBlock(parent=c, t='net') as net:
+                pass
+
+            for i, a1, a2 in zip(xrange(len(n1.addr)), n1.addrs, n2.addrs):
+                with ConfigBlock(parent=c, t='path') as path:
+                    self._config_one_host_addr(n1, path, i)
+                    self._config_one_host_addr(n2, path, i)
+
+    def _config_conns_9(self, parent):
+        for start, n1 in enumerate(self.resource.nodes):
+            for n2 in self.resource.nodes[start + 1:]:
+                self._config_one_connection(n1, n2, parent)
+
+    def config_host(self, node, parent, index):
+        resource = self.resource
+
+        with ConfigBlock(parent=parent, t='on %s' % node.hostname) as N:
+            if self.drbd_major_version == 9:
+                N.write("node-id %d;" % index)
+            else:
+                # 8.4 compat
+                N.write("address %s:%d;" % (node.addr, node.port))
+
+            for index, disk in enumerate(node.disks):
+                with ConfigBlock(parent=N, t='volume %d' % index) as V:
+                    V.write("device /dev/drbd%d;" % disk.minor)
+                    V.write("disk %s;" % (disk.disk or "none"))
+                    if disk.disk:
+                        V.write("meta-disk %s;" % (disk.meta or "internal"))
 
     def config(self):
+        text = ["global { usage-count no; }\n\n"]
+
         resource = self.resource
-        return subprocess.check_output(
-            ['m4', '-P', '-I', os.path.join(TOP, 'lib', 'm4'),
-             '-D', 'TMPL_DEFS=' + self.tmpl_defs(resource),
-             os.path.join(TOP, 'lib', 'm4', 'preamble.m4'),
-             resource.template])
+        with ConfigBlock(dest_fn=lambda x: text.append(x),
+                           t="resource %s" % resource.name) as res:
+
+            with ConfigBlock(parent=res, t='disk') as disk:
+                disk.write("disk-flushes no;")
+                disk.write("md-flushes no;")
+
+            with ConfigBlock(parent=res, t='net') as net:
+                net.write(resource.net_options)
+
+            for index, n in enumerate(resource.nodes):
+                self.config_host(n, parent=res, index=index)
+
+            if self.drbd_major_version == 8:
+                self._config_conns_84(res)
+            else:
+                self._config_conns_9(res)
+
+        return "".join(text)
 
     def update_config(self):
         """ Create or update the configuration file on the node when needed. """
@@ -959,9 +1026,10 @@ class Node(exxe.Exxe):
                 fio_extra = [ 'runtime=%d' % runtime ] + fio_extra,
                 )
 
-    def net_device_to_peer(self, peer):
+    def net_device_to_peer(self, peer, net_num=0):
         """Returns the network device this peer is reachable via."""
-        lines = self.run(['ip', '-o', 'route', 'get', peer.addr], return_stdout=True)
+        lines = self.run(['ip', '-o', 'route', 'get', peer.addrs[net_num]],
+                         return_stdout=True)
         fields = lines.split(' ')
         dev = fields[2]
         assert re.search(r'^eth', dev)
@@ -1029,7 +1097,7 @@ def scan_syslog_files(logdir):
 
 
 def setup(parser=argparse.ArgumentParser(),
-          nodes=None, max_nodes=None, min_nodes=2):
+          nodes=None, max_nodes=None, min_nodes=2, multi_paths=False):
     """
     Test setup.  Returns a resource object.
 
@@ -1046,7 +1114,6 @@ def setup(parser=argparse.ArgumentParser(),
     parser.add_argument('--cleanup', default='success',
                         choices=['success', 'always', 'never'])
     parser.add_argument('--volume-group', default='scratch')
-    parser.add_argument('--template')
     parser.add_argument('--vconsole', action='store_true')
     parser.add_argument('--silent', action='store_true')
     parser.add_argument('-v', action='count', dest='verbose')
@@ -1123,11 +1190,11 @@ def setup(parser=argparse.ArgumentParser(),
 
     Cleanup(args.cleanup)
     resource = Resource(args.resource,
-                        logdir=args.logdir,
-                        template=args.template)
+                        logdir=args.logdir)
 
     for node in args.node:
-        Node(resource, node, args.volume_group)
+        Node(resource, node, args.volume_group,
+                multi_paths=multi_paths)
 
     if args.vconsole:
         for node in args.node:

lib/instantiate-template
@@ -1,292 +1,0 @@
-#! /bin/bash
-
-: ${TOP:=${0%/*}/..}
-. $TOP/lib/params.sh
-. $TOP/lib/client.sh
-
-m4_define() {
-    local name array key
-    for name in "$@"; do
-	set -- $(declare -p "$name" 2> /dev/null)
-	case "$2" in
-	-A)
-	    # Asociative array
-	    echo "m4_define_array(\`$name')"
-	    eval "
-		for key in \"\${!$name[@]}\"; do
-		    echo \"$name(\\\`\$key', \\\`\${$name[\$key]}')\"
-		done
-	    "
-	    ;;
-	-a)
-	    # Array
-	    eval "set -- \"\${$name[@]}\""
-	    [ $# -eq 0 ] || set -- "$(printf "\`%s', " "$@")"
-	    echo "m4_define(\`$name', \`${1%??}')"
-	    ;;
-	--|-x)
-	    # Normal variable (exported or not)
-	    echo "m4_define(\`$name', \`${!name}')"
-	    ;;
-	*)
-	    echo "$0: Internal error" >&2
-	    exit 3
-	    ;;
-	esac
-    done
-}
-
-instantiate_template() {
-    local TMPL=$1
-    shift
-    local TMPL_DEFS="$(m4_define "$@")"
-
-    do_debug m4 -P -I $TOP/lib/m4/ \
-	-D TMPL_DEFS="$TMPL_DEFS" \
-	$TOP/lib/m4/preamble.m4 "$TMPL"
-}
-
-assign_addresses() {
-    local node address
-
-    for node in "${NODES[@]}"; do
-	if [ -z "${params[$node:ADDRESS]}" ]; then
-	    if address=$(gethostip -d "$node"); then
-		params[$node:ADDRESS]="$address:$opt_port"
-	    else
-		params[$node:ADDRESS]="$node:$opt_port"
-		status=1
-	    fi
-	fi
-    done
-}
-
-declare -a node_ids_used
-assign_node_ids() {
-    local node id n
-
-    for node in "${NODES[@]}"; do
-	id=${params["$node:NODE_ID"]}
-	if [ -n "$id" ]; then
-	    node_ids_used[$id]=1
-	fi
-    done
-
-    for node in "${NODES[@]}"; do
-	if [ -z "${params["$node:NODE_ID"]}" ]; then
-	    for ((n = 0; ; n++)); do
-		if [ -z "${node_ids_used[$n]}" ]; then
-		    node_ids_used[$n]=1
-		    break
-		fi
-	    done
-	    params["$node:NODE_ID"]=$n
-	fi
-    done
-}
-
-declare -A max_minor
-assign_devices() {
-    local node_name node name disk minor device device_node
-
-    for node_name in "${!params[@]}"; do
-	node=${node_name%%:*}
-	name=${node_name#*:}
-	case "$name" in
-	DISK*)
-	    device=${name/#DISK/DEVICE}
-	    if [ -z "${params[$device]}" ]; then
-		minor=$(( max_minor[$node]++ ))
-		set_node_param "$device" "$node" "/dev/drbd$minor"
-		set_node_param "${device/#DEVICE/MINOR}" "$node" "$minor"
-	    fi
-	esac
-    done
-}
-
-define_node_array() {
-    local n=0
-    for node in "${NODES[@]}"; do
-	NODE[$((n++))]=$node
-    done
-}
-
-defined() {
-    declare -p "$1" > /dev/null 2> /dev/null
-}
-
-usage() {
-    [ $1 -eq 0 ] || exec >&2
-    cat <<EOF
-USAGE: ${0##*/} [options] {template}
-
-OPTIONS:
-  --resource=name
-    Resource name to fill in (e.g., drbd0).  Required.
-
-  --node=name
-    Name of a test node.  This option can be used multiple times, and is
-    requird at least once.
-
-  --hostname=hostname
-    Host name of the test node according to \`hostname'.  Required.
-
-  --disk=device, --meta=device
-    The lower-level device to use for data or metadata.  When
-    no lower-level device is specified, the value /dev/scratch/\$resource
-    is used.  When no metadata device is specified, internal metadata
-    are assumed.  Both options can be used multiple times.  When used
-    before --node, they define defaults which can be overridden for each
-    host (after --node).
-
-  --address=address
-    The network address and port number of a node.  When
-    omitted, the first network address of the host according
-    to \`gethostip' is used.  Can only be used after --host.
-
-  --node-id=number
-    The node identifier of a node.  When omitted, node idenfiers
-    are assigned automatically.  Can only be used after --host.
-
-  --device=device
-    The upper-level device to use (e.g., /dev/drbd0).  When omitted,
-    /dev/drbd\$n devices are assigned automatically.
-
-  --port=number
-    The port number to use by default.
-
-  --sh-cfg=filename
-    Write the configuration into specified file in shell-parseable form.
-
-  --debug
-    Print the M4 command line used to produce the configuration file.
-EOF
-    exit $1
-}
-
-options=`getopt -o h --long resource:,node:,hostname:,device:,disk:,meta:,node-id:,address:,drbd-major-version:,debug,port:,sh-cfg:,help -- "$@"` || usage 1
-eval set -- "$options"
-
-declare opt_debug= opt_debug1=
-declare DRBD_MAJOR_VERSION= RESOURCE= opt_sh_cfg=
-declare -A NODE_ID ADDRESS
-opt_port=7789
-
-while :; do
-    case "$1" in
-    -h|--help)
-	usage 0
-	;;
-    --debug)
-	opt_debug=1
-	opt_debug1=1
-	;;
-    --port)
-	opt_port=$2
-	shift
-	;;
-    --drbd-major-version)
-	DRBD_MAJOR_VERSION=$2
-	shift
-	;;
-    --resource)
-	RESOURCE=$2
-	shift
-	;;
-    --node)
-	new_node "$2"
-	shift
-	;;
-    --hostname)
-	if [ "$node" = _ ]; then
-	    echo "Option $1 is not allowed before the first --node option" >&2
-	    exit 1
-	fi
-	set_node_param "$1" "$node" "$2"
-	shift
-	;;
-    --device|--disk|--meta)
-	add_node_param "$1" "$node" "$2"
-	shift
-	;;
-    --node-id|--address)
-	if [ "$node" = _ ]; then
-	    echo "Option $1 is not allowed before the first --node option" >&2
-	    exit 1
-	fi
-	set_node_param "$1" "$node" "$2"
-	shift
-	;;
-    --sh-cfg)
-	opt_sh_cfg=$2
-	shift
-	;;
-    --)
-	shift
-	break
-	;;
-    esac
-    shift
-done
-
-[ -n "$RESOURCE" -a ${#NODES[@]} -ge 1 -a $# -ge 1 ] || usage 1
-
-unset_generic_node_params
-
-assign_addresses
-assign_devices
-assign_node_ids
-
-unset HOSTNAME
-
-# Convert params[node:NAME]=value into NAME[node]=value representation
-for node_name in "${!params[@]}"; do
-    node=${node_name%%:*}
-    name=${node_name#*:}
-    eval "
-	declare -A $name
-	$name[\$node]=\"\${params[\"\$node:\$name\"]}\"
-    "
-done
-
-declare -A NODE
-define_node_array
-
-VOLUMES=(${!DISK*})
-VOLUMES=("${VOLUMES[@]/#DISK}")
-for template in "$@"; do
-    instantiate_template "$template" RESOURCE NODE NODES VOLUMES NODE_ID ADDRESS DRBD_MAJOR_VERSION HOSTNAME ${!DEVICE*} ${!DISK*} ${!META*}
-done
-
-if [ -n "$opt_sh_cfg" ]; then
-    {   echo "cfg[\"resources\"]=\"$RESOURCE\""
-	echo "cfg[\"$RESOURCE::nodes\"]=\"${NODES[*]}\""
-
-	for node in "${NODES[@]}"; do
-	    echo "cfg[\"$RESOURCE:$node::node-id\"]=\"${params["$node:NODE_ID"]}\""
-	    echo "cfg[\"$RESOURCE:$node::hostname\"]=\"${params[$node:HOSTNAME]}\""
-	    echo "cfg[\"$RESOURCE:$node::volumes\"]=\"${VOLUMES[@]}\""
-	    declare -a connections=()
-	    for volume in "${VOLUMES[@]}"; do
-		echo "cfg[\"$RESOURCE:$node:$volume::device\"]=\"${params[$node:DEVICE$volume]}\""
-		echo "cfg[\"$RESOURCE:$node:$volume::minor\"]=\"${params[$node:MINOR$volume]}\""
-		echo "cfg[\"$RESOURCE:$node:$volume::disk\"]=\"${params[$node:DISK$volume]}\""
-		echo "cfg[\"$RESOURCE:$node:$volume::meta\"]=\"${params[$node:META$volume]}\""
-	    done
-	    for node2 in "${NODES[@]}"; do
-		[ "$node" != "$node2" ] || continue
-		connections[${#connections[@]}]=$node2
-	    done
-	    echo "cfg[\"$RESOURCE:$node::connections\"]=\"${connections[*]}\""
-	    for node2 in "${NODES[@]}"; do
-		[ "$node" != "$node2" ] || continue
-		echo "cfg[\"$RESOURCE:$node:$node2::name\"]=\"${params[$node2:HOSTNAME]}\""
-		echo "cfg[\"$RESOURCE:$node:$node2::local\"]=\"${params[$node:ADDRESS]}\""
-		echo "cfg[\"$RESOURCE:$node:$node2::peer\"]=\"${params[$node2:ADDRESS]}\""
-	    done
-	    echo
-	done
-    } > "$opt_sh_cfg"
-fi
-
-exit $status

lib/m4/README
@@ -1,12 +1,0 @@
-The quote.m4, foreachq4.m4, forloop2.m4, and join.m4 macros in this directory
-were taken from the GNU M4 package with are released with the following
-copyright statement:
-
-The files in this directory provide example uses of GNU M4.
-The following copyright notice applies to each of these
-description files.
-
-Copyright (C) 2006, 2010 Free Software Foundation, Inc.
-This file is free software; the Free Software Foundation
-gives unlimited permission to copy and/or distribute it,
-with or without modifications, as long as this notice is preserved.

lib/m4/array.m4
@@ -1,10 +1,0 @@
-m4_divert(`-1')
-m4_define(`m4_set_array',
-  `m4_define(m4_format(``$1[%s]'', `$2'), `$3')')
-m4_define(`m4_get_array',
-  `m4_defn(m4_format(``$1[%s]'', `$2'))')
-m4_define(`m4_define_array',
-  `m4_define(`$1',`m4_ifelse'(m4_dquote(m4_quote(`$'`#'),1,`m4_get_array'(m4_dquote(`$1',m4_quote(`$'`1'))),`m4_set_array'(m4_dquote(`$1',m4_quote(`$'`1'),m4_quote(`$'`2'))))))')
-m4_define(`m4_next_array',
-  `m4_define(`$1',`m4_define'(m4_dquote(`next$1[$'`1]'),m4_quote(`m4_ifdef'(m4_dquote(`next$1[$'`1]'),m4_dquote(`m4_incr'(`m4_defn'(m4_dquote(`next$1[$'`1]')))),1)))`m4_get_array'(m4_quote(`m4_format'(```$1%s''',`m4_defn'(m4_dquote(`next$1[$'`1]')))),m4_dquote(`$'`1')))')
-m4_divert`'m4_dnl

lib/m4/default.m4
@@ -1,3 +1,0 @@
-m4_divert(-1)
-m4_define(`m4_default', `m4_ifelse(`$1', `', `$2', ``$1'')')
-divert`'dnl

lib/m4/foreachq4.m4
@@ -1,13 +1,0 @@
-m4_include(`forloop2.m4')m4_dnl
-m4_divert(`-1')
-# foreachq(x, `item_1, item_2, ..., item_n', stmt)
-#   quoted list, version based on forloop
-m4_define(`m4_foreachq',
-`m4_ifelse(`$2', `', `', `_$0(`$1', `$3', $2)')')
-m4_define(`_m4_foreachq',
-`m4_pushdef(`$1', m4_forloop(`$1', `3', `$#',
-  `$0_(`1', `2', m4_indir(`$1'))')`m4_popdef(
-    `$1')')m4_indir(`$1', $@)')
-m4_define(`_m4_foreachq_',
-``m4_define(`$$1', `$$3')$$2`''')
-m4_divert`'m4_dnl

lib/m4/forloop2.m4
@@ -1,12 +1,0 @@
-m4_divert(`-1')
-# forloop(var, from, to, stmt) - improved version:
-#   works even if VAR is not a strict macro name
-#   performs sanity check that FROM is larger than TO
-#   allows complex numerical expressions in TO and FROM
-m4_define(`m4_forloop', `m4_ifelse(m4_eval(`($2) <= ($3)'), `1',
-  `m4_pushdef(`$1')_$0(`$1', m4_eval(`$2'),
-    m4_eval(`$3'), `$4')m4_popdef(`$1')')')
-m4_define(`_m4_forloop',
-  `m4_define(`$1', `$2')$4`'m4_ifelse(`$2', `$3', `',
-    `$0(`$1', m4_incr(`$2'), `$3', `$4')')')
-m4_divert`'m4_dnl

lib/m4/join.m4
@@ -1,15 +1,0 @@
-m4_divert(`-1')
-# m4_join(sep, args) - join each non-empty ARG into a single
-# string, with each element separated by SEP
-m4_define(`m4_join',
-`m4_ifelse(`$#', `2', ``$2'',
-  `m4_ifelse(`$2', `', `', ``$2'_')$0(`$1', m4_shift(m4_shift($@)))')')
-m4_define(`_m4_join',
-`m4_ifelse(`$#$2', `2', `',
-  `m4_ifelse(`$2', `', `', ``$1$2'')$0(`$1', m4_shift(m4_shift($@)))')')
-# m4_joinall(sep, args) - join each ARG, including empty ones,
-# into a single string, with each element separated by SEP
-m4_define(`m4_joinall', ``$2'_$0(`$1', m4_shift($@))')
-m4_define(`_m4_joinall',
-`m4_ifelse(`$#', `2', `', ``$1$3'$0(`$1', m4_shift(m4_shift($@)))')')
-m4_divert`'m4_dnl

commit f35f026bd3071df202a7d0c8a22dda97dd03b919

drbdtest: fix typo "addr" by removing the need for it
"n1.host.addr" was incorrect. This presumably worked because there is a
field "addr" which contains a string which has always been longer than
the number of paths we tried to use.

Fixes: 997a3d176e221 ("Remove m4 config file generation with some python code.")

python/drbdtest.py
@@ -1813,10 +1813,10 @@ class Node():
         # done via "address" in "on <host>" section
         pass
 
-    def _config_one_host_addr(self, node, block, i):
+    def _config_one_host_addr(self, node, block, addr):
         block.write("host %s address %s:%d;" %
                     (node.name,
-                     node.host.addrs[i],
+                     addr,
                      node.port))
 
     def _config_one_connection(self, n1, n2):
@@ -1839,10 +1839,10 @@ class Node():
                 with ConfigBlock(t='net'):
                     pass
 
-                for i, a1, a2 in zip(range(len(n1.host.addr)), n1.host.addrs, n2.host.addrs):
+                for a1, a2 in zip(n1.host.addrs, n2.host.addrs):
                     with ConfigBlock(t='path') as path:
-                        self._config_one_host_addr(n1, path, i)
-                        self._config_one_host_addr(n2, path, i)
+                        self._config_one_host_addr(n1, path, a1)
+                        self._config_one_host_addr(n2, path, a2)
 
     def _config_conns_9(self):
         for start, n1 in enumerate(self.resource.nodes):
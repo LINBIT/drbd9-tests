995ad8e5c2b39452bf7c7aa69ba0afc5ee5f1fe7
Joel Colledge <joel.colledge@linbit.com>
2024-03-13 19:08:39 +0100

    trafficcontrol: apply to all networks

--- python/trafficcontrol.py
+++ python/trafficcontrol.py
@@ -15,7 +15,7 @@ class TrafficControl(object):
         self.source_node = source_node
         self.nodes = nodes
 
-        for dev, nodes in self._get_nodes_by_dev().items():
+        for dev, (net_num, nodes) in self._get_nodes_by_dev().items():
             # Clear away any leftover mess
             try:
                 self.source_node.run(['tc', 'qdisc', 'del', 'dev', dev, 'root'])
@@ -32,9 +32,9 @@ class TrafficControl(object):
                 'priomap'] + [str(self.source_node.id)] * 16)
 
             for node in nodes:
-                ip = node.host.addr
-                log('Traffic control from {0} to {1} uses {2} ({3})'
-                        .format(self.source_node, node, dev, ip))
+                ip = node.host.addrs[net_num]
+                log('Traffic control for net {0} from {1} to {2} uses {3} ({4})'
+                        .format(net_num, self.source_node, node, dev, ip))
 
                 # Filter packets to the destination IP and send them to the corresponding class.
                 self.source_node.run(['tc', 'filter', 'add', 'dev', dev,
@@ -50,35 +50,42 @@ class TrafficControl(object):
             self.source_node.run(['tc', 'qdisc', 'del', 'dev', dev, 'root'])
 
     def slow_down(self, to_node, speed='', delay=''):
-        dev = self.source_node.host.net_device_to_peer(to_node.host)
-        log('Slowing down connection from {0} to {1}'.format(self.source_node, to_node))
+        for net_num in range(len(self.source_node.host.addrs)):
+            dev = self.source_node.host.net_device_to_peer(to_node.host, net_num)
+            log('Slowing down connection for net {0} from {1} to {2}'.format(net_num, self.source_node, to_node))
 
-        netem_args = []
-        if delay != '':
-            netem_args += ['delay', delay]
-        if speed != '':
-            netem_args += ['rate', speed]
+            netem_args = []
+            if delay != '':
+                netem_args += ['delay', delay]
+            if speed != '':
+                netem_args += ['rate', speed]
 
-        # Add our netem qdisc as the child of the corresponding class of the 'prio' qdisc.
-        self.source_node.run(['tc', 'qdisc', 'replace', 'dev', dev,
-            'parent', self._node_to_class(to_node),
-            'handle', self._node_to_handle(to_node),
-            'netem'] + netem_args)
+            # Add our netem qdisc as the child of the corresponding class of the 'prio' qdisc.
+            self.source_node.run(['tc', 'qdisc', 'replace', 'dev', dev,
+                'parent', self._node_to_class(to_node),
+                'handle', self._node_to_handle(to_node),
+                'netem'] + netem_args)
 
     def remove_slow_down(self, to_node):
-        dev = self.source_node.host.net_device_to_peer(to_node.host)
-        self.source_node.run(['tc', 'qdisc', 'del', 'dev', dev,
-            'parent', self._node_to_class(to_node),
-            'handle', self._node_to_handle(to_node)])
+        for net_num in range(len(self.source_node.host.addrs)):
+            dev = self.source_node.host.net_device_to_peer(to_node.host, net_num)
+            self.source_node.run(['tc', 'qdisc', 'del', 'dev', dev,
+                'parent', self._node_to_class(to_node),
+                'handle', self._node_to_handle(to_node)])
 
     def _get_nodes_by_dev(self):
         nodes_by_dev = {}
-        for node in self.nodes:
-            if node != self.source_node:
-                dev = self.source_node.host.net_device_to_peer(node.host)
-                if not dev in nodes_by_dev:
-                    nodes_by_dev[dev] = []
-                nodes_by_dev[dev].append(node)
+        for net_num in range(len(self.source_node.host.addrs)):
+            for node in self.nodes:
+                if node != self.source_node:
+                    dev = self.source_node.host.net_device_to_peer(node.host, net_num)
+                    if not dev in nodes_by_dev:
+                        nodes_by_dev[dev] = (net_num, [])
+                    dev_net_num, nodes = nodes_by_dev[dev]
+                    if dev_net_num != net_num:
+                        raise RuntimeError('{}: network {} and {} use same device {}'
+                                .format(node, net_num, dev_net_num, dev))
+                    nodes.append(node)
         return nodes_by_dev
 
     @staticmethod

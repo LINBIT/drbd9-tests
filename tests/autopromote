LINBIT/drbd9-tests

commit c68c9d57762d10c53fcf67e07fe63bfa514b0947

replace open coded calls to setsid and fio with busywrite

tests/autopromote
@@ -9,6 +9,7 @@ import time
 from python import drbdtest
 from python.drbdtest import connections, log, peer_devices
 from python import trafficcontrol
+from python import busywrite
 
 READ_ONLY = '<'
 READ_WRITE = '>'
@@ -60,13 +61,11 @@ node_b.secondary()
 # This was broken from DRBD 9.1.7 to 9.1.16 inclusive.
 log('* Test auto-demote on SIGTERM.')
 a_tc.slow_down(node_b, delay='200ms')
-opener_pid = node_a.run([
-    "setsid", "bash", "-c",
-    "dd if=/dev/zero of={} bs=4K count=1 oflag=direct < /dev/null &> /dev/null & echo $!".format(node_a.volumes[0].device())],
-    return_stdout=True)
+opener = busywrite.BusyWrite(node_a.volumes[0])
+opener.start(fio_base_args='--rw=write --size=4k --direct=1')
 # Allow time for auto-promote, but not for write to complete.
 time.sleep(0.5)
-node_a.run(["kill", "-TERM", opener_pid])
+opener.stop()
 node_a.event(r'resource .* role:Secondary')
 
 log('* Shut down and clean up.')

tests/promote_while_connecting
@@ -8,6 +8,7 @@
 
 from python import drbdtest
 from python.drbdtest import connections, log, peer_devices
+from python import busywrite
 import time
 #import os
 
@@ -25,7 +26,6 @@ B = resource.nodes[1]
 a_b = connections(A)
 b_a = connections(B)
 pds = drbdtest.PeerDevices.from_connections(resource.connections)
-dev_name = A.volumes[0].device()
 
 my_log('* Make up-to-date data available.')
 resource.skip_initial_sync()
@@ -39,16 +39,14 @@ A.primary()
 a_b.connect()
 time.sleep(0.8)
 my_log('* Creating new UUID while uuid_sem is locked.')
-dd_pid = A.run([
-    "setsid", "bash", "-c",
-    "dd if=/dev/zero of=%s bs=%dk count=1 oflag=direct < /dev/null &> /dev/null & echo $!" % (dev_name, 4)],
-    return_stdout=True)
+writer = busywrite.BusyWrite(A.volumes[0])
+writer.start(fio_base_args='--rw=write --size=4k --direct=1')
 A.unblock_packet_type(drbdtest.P_SIZES, from_node=B)
 
 pds.event(r'peer-device .* replication:Established')
 
-log('* Wait for dd process to finish')
-A.run(["tail", "--pid={}".format(dd_pid), "-f", "/dev/null"])
+log('* Wait for background fio process to finish')
+writer.wait()
 
 log('* Shut down and clean up.')
 

tests/quorum-isolate-primary
@@ -23,6 +23,7 @@
 import time
 from python import drbdtest
 from python.drbdtest import connections, log, peer_devices
+from python import busywrite
 from subprocess import CalledProcessError
 
 
@@ -51,8 +52,8 @@ def test_write_between_disconnects(resource):
     # that the cut2 secondary can still ping the primary when cut1 already
     # wants to reconciliate with cut2.
 
-    dev_name = primary_n.volumes[0].device()
-    primary_n.run(["setsid", "bash", "-c", "dd if=/dev/zero of=%s bs=%dk count=1 oflag=direct < /dev/null &> /dev/null &" % (dev_name, 4)])
+    writer = busywrite.BusyWrite(primary_n.volumes[0])
+    writer.start(fio_base_args='--rw=write --size=4k --direct=1')
     connection_witness1.event(r'connection .* connection:NetworkFailure')
 
     log('* silently cutting off other secondary (%s) from primary' % (cut2_sec.name))

tests/quorum-suspend-timeout
@@ -22,6 +22,7 @@
 import time
 from python import drbdtest
 from python.drbdtest import connections, log, peer_devices
+from python import busywrite
 
 def do_verify(node_source, node_target):
     node_source.drbdadm(['verify', '-v', '--start=0', '%s:%s/0' % (resource.name, node_target.name)])
@@ -46,9 +47,8 @@ def prepare_test(primary_n, freezing_io_n):
     resource.forbidden_patterns.remove(r'connection:BrokenPipe')
     resource.forbidden_patterns.remove(r'connection:NetworkFailure')
     resource.forbidden_patterns.remove(r'connection:Timeout')
-    primary_n.run(['setsid', 'bash', '-c',
-                   'fio --max-jobs=1 --name=test --filename={} --bs=4K --ioengine=libaio --iodepth=24 --rw=write --size=2M --direct=1 < /dev/null &> /dev/null & echo $!'
-                   .format(vol.device())], return_stdout=True)
+    writer = busywrite.BusyWrite(vol)
+    writer.start(fio_base_args='--rw=write --size=2M --iodepth=24 --direct=1')
 
     primary_suspended_c.event(r'connection .* connection:Timeout')
     suspended_primary_c.event(r'connection .* connection:(BrokenPipe|NetworkFailure)')

tests/quorum-suspended-cancel
@@ -24,19 +24,12 @@
 
 from python import drbdtest
 from python.drbdtest import connections, log, peer_devices
+from python import busywrite
 import time
 
-def write_on_node(node):
-    v = node.volumes[0]
-    # write 4K in background
-    pid = primary_n.run(['setsid', 'bash', '-c',
-                   'dd if=/dev/urandom of={0} bs=4096 oflag=direct count=1 < /dev/null &> /dev/null & echo $!'
-                   .format(v.device())], return_stdout=True)
-
-    return pid
 
 def do_test(primary_n, other_nodes, wait_before_write):
-    dd_pid = 0
+    writer = busywrite.BusyWrite(primary_n.volumes[0])
     primary_conn = connections(from_nodes=other_nodes, to_node=primary_n)
 
     # cut connection so that primary loses quorum
@@ -45,13 +38,13 @@ def do_test(primary_n, other_nodes, wait_before_write):
 
     # now write on the primary without quorum; this should block
     if not wait_before_write:
-        dd_pid = write_on_node(primary_n)
+        writer.start(fio_base_args='--rw=write --size=4k --direct=1')
 
     primary_conn.event(r'connection:(NetworkFailure|Timeout)')
     primary_n.event(r'device .* quorum:no')
 
     if wait_before_write:
-        dd_pid = write_on_node(primary_n)
+        writer.start(fio_base_args='--rw=write --size=4k --direct=1')
 
     # give it some time to actually block
     time.sleep(1)
@@ -59,8 +52,8 @@ def do_test(primary_n, other_nodes, wait_before_write):
     # set on-no-quorum policy
     primary_n.run(['drbdsetup', 'resource-options', resource.name, '--on-no-quorum=io-error'])
 
-    # wait for the dd command to exit
-    primary_n.run(['tail', '--pid={0}'.format(dd_pid), '-f', '/dev/null'])
+    # wait for the fio command to exit
+    writer.wait()
 
     for n in other_nodes:
         primary_n.unblock_path(n, 0)

commit 59312e9d89df350331e282c415b33a9b15f73730

autopromote: restore stability and effectiveness
Commit c68c9d57762d ("replace open coded calls to setsid and fio with
busywrite") caused the test autopromote to become less reliable. The
test relied on fragile timing. With the switch to use fio instead of dd,
the time starting the process became longer, so that the signal was
often sent too early.

In addition, fio opens and closes the device once to perform the
BLKGETSIZE64 ioctl before it opens the device to actually write. As a
result, DRBD must auto-promote and auto-demote twice. This could cause
an event which makes the test pass even when the bug is present.

Fix these issues by switching from network delay to packet blocking.
This is a more robust way of controlling the timing. There are still
some sleeps involved, but these are less sensitive than the sleep in the
previous solution.

Fixes: c68c9d57762d ("replace open coded calls to setsid and fio with busywrite")

python/busywrite.py
@@ -65,6 +65,12 @@ class BusyWrite(object):
         with open(os.path.join(self._node.resource.cluster.logdir, self._output_filename), 'w') as output_file:
             output_file.write(self._fio_out_str)
 
+    def kill_jobs(self):
+        if self._fio_pid is None:
+            raise RuntimeError('Start first')
+
+        self._node.run(['bash', '-c', 'kill $(ps --ppid {} -o pid=)'.format(self._fio_pid)])
+
     def stop(self):
         if self._fio_pid is None:
             raise RuntimeError('Start first')

tests/autopromote
@@ -5,10 +5,10 @@
 # conneciton loss to the peer (I.e. before the local node
 # noticed the absence of the peer).
 
+import json
 import time
 from python import drbdtest
 from python.drbdtest import connections, log, peer_devices
-from python import trafficcontrol
 from python import busywrite
 
 READ_ONLY = '<'
@@ -27,8 +27,6 @@ def test_open(node, mode, expect):
 resource = drbdtest.setup_resource(nodes=2)
 node_a, node_b = resource.nodes
 resource.add_disk('10M')
-
-a_tc = trafficcontrol.TrafficControl(node_a, resource.nodes)
 
 log('* Make up-to-date data available.')
 resource.up_wait()
@@ -60,15 +58,34 @@ node_b.secondary()
 
 # This was broken from DRBD 9.1.7 to 9.1.16 inclusive.
 log('* Test auto-demote on SIGTERM.')
-a_tc.slow_down(node_b, delay='200ms')
+# Prevent ping timeouts while the control stream is blocked
+resource.net_options = 'ping-timeout 40;'
+resource.nodes.adjust()
+
+node_a.block_packet_type(drbdtest.P_WRITE_ACK, from_node=node_b)
 opener = busywrite.BusyWrite(node_a.volumes[0])
 opener.start(fio_base_args='--rw=write --size=4k --direct=1')
-# Allow time for auto-promote, but not for write to complete.
+
+# Allow time for auto-promote; write is blocked
 time.sleep(0.5)
-opener.stop()
-node_a.event(r'resource .* role:Secondary')
+
+# Unblock write; allow time for auto-demote to start, but block it
+node_a.block_packet_type(drbdtest.P_TWOPC_YES, from_node=node_b, volume=0xffff)
+node_a.unblock_packet_type(drbdtest.P_WRITE_ACK, from_node=node_b)
+time.sleep(1.0)
+
+# Terminate during auto-demote; signal must be sent to the exact process that
+# is in the close() syscall
+opener.kill_jobs()
+node_a.unblock_packet_type(drbdtest.P_TWOPC_YES, from_node=node_b, volume=0xffff)
+opener.wait()
+
+# Check current status instead of events because fio opens and closes the
+# device multiple times, causing multiple promotes and demotes
+result = node_a.run(['drbdsetup', 'status', resource.name, '--json'], return_stdout=True)
+status = json.loads(result)[0]
+drbdtest.ensure('Secondary', status['role'])
 
 log('* Shut down and clean up.')
 resource.down()
-a_tc.reset()
 resource.cluster.teardown()
a529f496936a9818012d6aea16be2b917a13e15b
Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>
2024-07-05 11:25:48 +0200

    vms: use real RHEL7 VMs

--- virter/scripts/inject-rhel-secrets.sh
+++ virter/scripts/inject-rhel-secrets.sh
@@ -21,7 +21,9 @@ if [ -z "$entitlement_key" ] || [ -z "$entitlement_cert" ]; then
 	die "Entitlement key or cert not found."
 fi
 
-cat > /run/secrets/rhel.repo <<EOF
+if command -v dnf; then
+	# Has DNF command, so this is RHEL8+ like
+	cat > /run/secrets/rhel.repo <<EOF
 [rhel-baseos]
 name = Red Hat Enterprise Linux \$releasever for x86_64 - BaseOS (RPMs)
 baseurl = https://cdn.redhat.com/content/dist/rhel$dist_major_version/\$releasever/x86_64/baseos/os
@@ -66,7 +68,54 @@ metadata_expire = 86400
 enabled_metadata = 1
 sslverifystatus = 1
 EOF
+else
+	# Is RHEL7, renamed the repos so they follow the same scheme as RHEL8+
+	cat > /run/secrets/rhel.repo <<EOF
+[rhel-baseos]
+name = Red Hat Enterprise Linux \$releasever Server (RPMs)
+baseurl = https://cdn.redhat.com/content/dist/rhel/server/7/\$releasever/\$basearch/os
+enabled = 1
+gpgcheck = 1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+sslverify = 1
+sslcacert = /run/secrets/rhsm/ca/redhat-uep.pem
+sslclientkey = $entitlement_key
+sslclientcert = $entitlement_cert
+metadata_expire = 86400
+enabled_metadata = 1
+sslverifystatus = 1
 
+[rhel-appstream]
+name = Red Hat Enterprise Linux \$releasever Server - Optional (RPMs)
+baseurl = https://cdn.redhat.com/content/dist/rhel/server/7/\$releasever/\$basearch/optional/os
+enabled = 1
+gpgcheck = 1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+sslverify = 1
+sslcacert = /run/secrets/rhsm/ca/redhat-uep.pem
+sslclientkey = $entitlement_key
+sslclientcert = $entitlement_cert
+metadata_expire = 86400
+enabled_metadata = 1
+sslverifystatus = 1
+
+[rhel-baseos-eus]
+name = Red Hat Enterprise Linux 7 Server - Extended Life Cycle Support (RPMs)
+baseurl = https://cdn.redhat.com/content/els/rhel/server/7/\$releasever/\$basearch/os
+enabled = 1
+gpgcheck = 1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+sslverify = 1
+sslcacert = /run/secrets/rhsm/ca/redhat-uep.pem
+sslclientkey = $entitlement_key
+sslclientcert = $entitlement_cert
+metadata_expire = 86400
+enabled_metadata = 1
+sslverifystatus = 1
+skip_if_unavailable = True
+priority = 100 # weaker priority than the "normal" baseos repo so that userspace components are not taken from EUS
+EOF
+fi
 # The repo file is created in /run (which is a tmpfs) to avoid accidentally
 # leaking the secrets with the built base images. Obviously, this means that we
 # need to symlink the repo to /etc/... so that dnf finds it.

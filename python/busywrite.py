LINBIT/drbd9-tests

commit fd21d380780e619d9898ab8a65b143509dd837ec

WinDRBD and MS Windows support
This commit adds support for WinDRBD to the DRBD9 testsuite.
Notable changes are:

 * There are platform specific parts of the drbdtest.py library
   which have been moved into separate python modules (linux/
   windows platform helpers).

 * There is support for MS Storage Spaces storage pools.

 * The diskless test has been patch as a sample for how to
   make more tests work with WinDRBD

 * There are now windows and cross-platform variants. If you
   just want Linux tests, use the tcp variant (for example).

 * There is a machine description in the virter/vms.toml for
   a Windows Server 2019 VM.

 * Also, several files have been patched to support ssh config
   file created by virter. This file contains user names for
   the VMs created by virter (Windows 'root' user is called
   'Administrator')

By default only Linux tests are run. To run Windows tests add
--variant=windows to the vmshed arguments.

python/busywrite.py
@@ -31,10 +31,15 @@ class BusyWrite(object):
         """
         self._output_filename = 'fio-{}-{}-async.json'.format(self._node.name, self._node.host.fio_count)
 
+        if self._node.host.is_linux_host():
+            platform_args = '--ioengine=libaio '
+        else:
+            platform_args = '--ioengine=windowsaio --thread '
+
         # run fio in background
         fio_cmd = ['setsid', 'bash', '-c',
-                'fio --output-format=json --max-jobs=16 --name=test --ioengine=libaio ' +
-                '--filename={} '.format(self._volume.device()) +
+                'fio --output-format=json --name=test --max-jobs=4 ' + platform_args +
+                '--filename={} '.format(self._node.host.native_filename(self._volume.device()).replace(":", "\\:").replace("\\", "\\\\")) +
                 fio_base_args + ' ' + fio_arg_str +
                 ' < /dev/null > /tmp/{} 2> /dev/null & echo $!'.format(self._output_filename)]
         self._fio_pid = self._node.run(fio_cmd, return_stdout=True)

.gitignore
@@ -11,3 +11,6 @@ tests/log/
 
 python/__pycache__/
 python/*.py[cod]
+
+virter/tests.toml
+tests-out

Makefile
@@ -11,7 +11,8 @@ VMSHED_TEST_TIMEOUT ?=
 docker:
 	docker build --no-cache=$(NOCACHE) -t $(DOCKER_IMAGE_NAME) docker/
 
-virter/tests.toml: tests/* virter/vmshed_tests_generator.py
+# make sure the DRBD_VERSION environment variable is set
+virter/tests.toml: tests/* virter/vmshed_tests_generator.py virter/variants.toml
 	virter/vmshed_tests_generator.py \
 		--tests-dir "$(DRBD_TESTS_DIR)" \
 		--selection "$(VMSHED_TEST_SELECTION)" \

docker/Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu:jammy
 
-ARG LBPYTEST_VERSION=0.3.0
+ARG LBPYTEST_VERSION=0.4.0
 
 RUN apt-get update && apt-get -y install \
     python3 \

docker/entry.sh
@@ -19,6 +19,11 @@ echo "===== Get the FQDN from the test nodes"
 # Read TARGETS into array targets without messing up IFS
 IFS=, read -a targets <<< "$TARGETS"
 
+test_args=( "$@" "--logdir" "/log" )
+if [ -f /etc/ssh/ssh_config.virter ] ; then
+    test_args+=( "--ssh-config=/etc/ssh/ssh_config.virter" )
+fi
+
 nodes=()
 for t in "${targets[@]}"; do
     if [ -f /etc/ssh/ssh_config.virter ] ; then
@@ -30,7 +35,6 @@ for t in "${targets[@]}"; do
     nodes+=( "$t_host" )
 done
 
-test_args=( "$@" "--logdir" "/log" )
 [ -n "$DRBD_VERSION" ] && test_args+=( "--drbd-version=$DRBD_VERSION" )
 [ -n "$DRBD_VERSION_OTHER" ] && test_args+=( "--drbd-version-other=$DRBD_VERSION_OTHER" )
 [ -n "$DRBD_OTHER_NODE" ] && test_args+=( "--drbd-other-node=$DRBD_OTHER_NODE" )

python/disktools.py
@@ -20,6 +20,8 @@ def create_storage_pool(host, backend, backing_device, thin=False, discard_granu
         pool = RawPool(host, backing_device)
     elif backend == 'zfs':
         pool = ZfsPool(host, backing_device, thin, discard_granularity)
+    elif backend == 'storage-spaces':
+        pool = StorageSpacesPool(host, backing_device, thin)
     else:
         raise NotImplementedError('backend "{}" not implemented'.format(backend))
 
@@ -87,6 +89,62 @@ class LvmVolume(object):
         lvs_rep = json.loads(json_str)
 
         return float(lvs_rep['report'][0]['lv'][0]['data_percent'])
+
+class StorageSpacesPool(object):
+    def __init__(self, host, backing_device, thin):
+        self._host = host
+        self._backing_device = backing_device
+        self._thin = thin
+
+    def remove(self):
+        pass
+
+    def create_disk(self, name, size, *, max_size=None):
+        return StorageSpacesVolume(self._host, name, size, self._thin, self._backing_device)
+
+class StorageSpacesVolume(object):
+    @classmethod
+    def size_in_bytes(cls, size):
+        size_str = str(size)
+        unit = size_str[-1:].upper()
+        if unit >= '0' and unit <= '9':
+            return int(size_str)
+
+        size_int = int(size_str[:-1])
+
+        exponent_table = { 'K': 1, 'M': 2, 'G': 3, 'T': 4, 'P': 5 }
+        exponent = exponent_table.get(unit)
+        if not exponent:
+            raise ValueError('Invalid storage size unit in ' + size_str)
+
+        return size_int * 1024 ** exponent
+
+    def __init__(self, host, name, size, thin, backing_device):
+        self._host = host
+        self._name = name
+        self._backing_device = backing_device
+
+        if thin:
+            thin_param = 'thin'
+        else:
+            thin_param = 'thick'
+
+        self._host.run(['./linstor-wmi-helper', 'virtual-disk', 'create', self._backing_device, name, str(StorageSpacesVolume.size_in_bytes(size)), thin_param])
+        line = self._host.run(['bash', '-c', './linstor-wmi-helper virtual-disk list '+name+' 2>&1'], return_stdout=True)
+        print("output is "+line)
+        self._lv = line.split('\t')[4].replace('{', '').replace('}', '')
+
+    def volume_path(self):
+        return self._lv
+
+    def remove(self):
+        self._host.run(['./linstor-wmi-helper', 'virtual-disk', 'delete',  self._name])
+
+    def resize(self, size):
+        self._host.run(['./linstor-wmi-helper', 'virtual-disk', 'resize',  self._name, str(self.size_in_bytes(size))])
+
+    def fill_percentage(self):
+        raise NotImplementedError('storage pool storage volume does not support fill_percentage (yet)')
 
 
 class RawPool(object):

python/drbdtestlogger.py
@@ -1,0 +1,62 @@
+import sys
+
+# stream to write output to
+logstream = None
+
+debug_level = 0
+
+class Tee(object):
+    """
+    replicates writes to streams
+    """
+
+    def __init__(self):
+        self.streams = set()
+
+    def add(self, stream):
+        # Do not modify self.streams in place. Another thread may be iterating over it.
+        streams = set(self.streams)
+        streams.add(stream)
+        self.streams = streams
+
+    def remove(self, stream):
+        # Do not modify self.streams in place. Another thread may be iterating over it.
+        streams = set(self.streams)
+        streams.remove(stream)
+        self.streams = streams
+
+    def write(self, message):
+        for stream in self.streams:
+            stream.write(message)
+
+    def flush(self):
+        for stream in self.streams:
+            stream.flush()
+
+def open_logstream(fname):
+    logfile = open(fname, 'w', encoding='utf-8')
+    # no need to close logfile - it is kept open until the program terminates
+    global logstream
+    logstream = Tee()
+    logstream.add(sys.stderr)
+    logstream.add(logfile)
+
+def log(*args, **kwargs):
+    """ Print message to stderr """
+    print(*args, file=logstream)
+    logstream.flush()
+
+def debug(*args, **kwargs):
+    """ Print debug message according to configured debug level. """
+
+    level = 1
+    try:
+        level = kwargs.pop('level')
+    except:
+        pass
+    if level <= debug_level:
+        print(*args, file=logstream)
+
+def set_debug_level(level):
+    global debug_level
+    debug_level = level

commit 268a113e3d3af5f01e1fc3c7df0274285caed850

busywrite: swap --name and --max-jobs back
The version of fio we use on RHEL 8 appears to not respect the global
--max-jobs option when it is placed after the --name option. This causes
fio to sometimes fail to start.

Fixes: fd21d380780e ("WinDRBD and MS Windows support")

python/busywrite.py
@@ -38,7 +38,7 @@ class BusyWrite(object):
 
         # run fio in background
         fio_cmd = ['setsid', 'bash', '-c',
-                'fio --output-format=json --name=test --max-jobs=4 ' + platform_args +
+                'fio --output-format=json --max-jobs=4 --name=test ' + platform_args +
                 '--filename={} '.format(self._node.host.native_filename(self._volume.device()).replace(":", "\\:").replace("\\", "\\\\")) +
                 fio_base_args + ' ' + fio_arg_str +
                 ' < /dev/null > /tmp/{} 2> /dev/null & echo $!'.format(self._output_filename)]